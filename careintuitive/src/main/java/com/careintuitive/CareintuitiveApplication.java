5ee9ebd53748ac272c2c0d1197e95e16a475c782
Carlos Henrique <ichfs9108@gmail.com>
2025-03-29 22:13:09 -0300

    project created

---
+++ careintuitive/src/main/java/com/careintuitive/CareintuitiveApplication.java
@@ -0,0 +1,13 @@
+package com.careintuitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareintuitiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareintuitiveApplication.class, args);
+	}
+
+}
---
+++ careintuitive/src/test/java/com/careintuitive/CareintuitiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careintuitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareintuitiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
